0c1f1cc219202efec855b30e32dc792b9a58f222
eggimann547 <52585504+eggimann547@users.noreply.github.com>
2025-11-15 16:15:07 -0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Turbopack is not available in Next.js 14.1.4
+  // This block is safe to remove
+};
+
+module.exports = nextConfig;
